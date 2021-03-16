9317432bb5d436c7dea2609b4a842c56a5fad888
Andrew Sears <azsears25@gmail.com>
2021-03-16 17:56:28 -0400

    Adding follower Talons and formatting code

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.TalonSRXSimCollection;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
@@ -29,8 +30,6 @@ import edu.wpi.first.wpilibj.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpiutil.math.VecBuilder;
 
-
-
 public class DriveSubsystem extends SubsystemBase {
 
   private WPI_TalonSRX leftTalon1 = new WPI_TalonSRX(1);
@@ -40,30 +39,28 @@ public class DriveSubsystem extends SubsystemBase {
   private WPI_TalonSRX rightTalon2 = new WPI_TalonSRX(5);
   private WPI_TalonSRX rightTalon3 = new WPI_TalonSRX(6);
 
- // The motors on the left side of the drive.
- private final SpeedControllerGroup m_leftMotors =
- new SpeedControllerGroup(leftTalon1,leftTalon2,leftTalon3);
+  // The motors on the left side of the drive.
+  private final SpeedControllerGroup m_leftMotors = new SpeedControllerGroup(leftTalon1, leftTalon2, leftTalon3);
 
-// The motors on the right side of the drive.
-private final SpeedControllerGroup m_rightMotors =
-new SpeedControllerGroup(rightTalon1,rightTalon2,rightTalon3);
+  // The motors on the right side of the drive.
+  private final SpeedControllerGroup m_rightMotors = new SpeedControllerGroup(rightTalon1, rightTalon2, rightTalon3);
 
   // The robot's drive
   private final DifferentialDrive m_drive = new DifferentialDrive(m_leftMotors, m_rightMotors);
-  
-  // The left-side drive encoder
-//  private final Encoder m_leftEncoder =
-//       new Encoder(
-//           DriveConstants.kLeftEncoderPorts[0],
-//           DriveConstants.kLeftEncoderPorts[1],
-//           DriveConstants.kLeftEncoderReversed);
 
-//  // The right-side drive encoder
-//   private final Encoder m_rightEncoder =
-//       new Encoder(
-//           DriveConstants.kRightEncoderPorts[0],
-//           DriveConstants.kRightEncoderPorts[1],
-//           DriveConstants.kRightEncoderReversed);
+  // The left-side drive encoder
+  // private final Encoder m_leftEncoder =
+  // new Encoder(
+  // DriveConstants.kLeftEncoderPorts[0],
+  // DriveConstants.kLeftEncoderPorts[1],
+  // DriveConstants.kLeftEncoderReversed);
+
+  // // The right-side drive encoder
+  // private final Encoder m_rightEncoder =
+  // new Encoder(
+  // DriveConstants.kRightEncoderPorts[0],
+  // DriveConstants.kRightEncoderPorts[1],
+  // DriveConstants.kRightEncoderReversed);
 
   // The gyro sensor
   private final ADXRS450_Gyro m_gyro = new ADXRS450_Gyro();
@@ -73,16 +70,18 @@ new SpeedControllerGroup(rightTalon1,rightTalon2,rightTalon3);
 
   // These classes help us simulate our drivetrain
   public DifferentialDrivetrainSim m_drivetrainSimulator;
- // private EncoderSim m_leftEncoderSim;
+  // private EncoderSim m_leftEncoderSim;
   // private EncoderSim m_rightEncoderSim;
   private TalonSRXSimCollection leftTalonSim;
   // The Field2d class shows the field in the sim GUI
   private Field2d m_fieldSim;
   private ADXRS450_GyroSim m_gyroSim;
 
-  final int kCountsPerRev = 2048;  //Encoder counts per revolution of the motor shaft.
-  final double kSensorGearRatio = 1; //Gear ratio is the ratio between the *encoder* and the wheels.  On the AndyMark drivetrain, encoders mount 1:1 with the gearbox shaft.
-  final double kGearRatio = 8.33; //Switch kSensorGearRatio to this gear ratio if encoder is on the motor instead of on the gearbox.
+  final int kCountsPerRev = 2048; // Encoder counts per revolution of the motor shaft.
+  final double kSensorGearRatio = 1; // Gear ratio is the ratio between the *encoder* and the wheels. On the AndyMark
+                                     // drivetrain, encoders mount 1:1 with the gearbox shaft.
+  final double kGearRatio = 8.33; // Switch kSensorGearRatio to this gear ratio if encoder is on the motor instead
+                                  // of on the gearbox.
   final double kWheelRadiusInches = 3;
   final int k100msPerSecond = 10;
 
@@ -91,29 +90,62 @@ new SpeedControllerGroup(rightTalon1,rightTalon2,rightTalon3);
 
   /** Creates a new DriveSubsystem. */
   public DriveSubsystem() {
+    leftTalon1.configFactoryDefault();
+    leftTalon2.configFactoryDefault();
+    leftTalon3.configFactoryDefault();
+    rightTalon1.configFactoryDefault();
+    rightTalon2.configFactoryDefault();
+    rightTalon3.configFactoryDefault();
+
+    leftTalon2.follow(leftTalon1);
+    leftTalon3.follow(leftTalon1);
+    rightTalon2.follow(rightTalon1);
+    rightTalon3.follow(rightTalon1);
+
+    leftTalon2.setInverted(InvertType.FollowMaster);
+    leftTalon3.setInverted(InvertType.FollowMaster);
+    rightTalon2.setInverted(InvertType.FollowMaster);
+    rightTalon3.setInverted(InvertType.FollowMaster);
+
+    // These "Real" settings should be changed to match your robot.
+    if (RobotBase.isReal()) { // We don't know if this is correct (wrong values), and w
+      // On our real robot, the left side is positive forward and
+      // sensor is in phase by default, so don't change it
+      leftTalon1.setInverted(InvertType.None);
+      leftTalon1.setSensorPhase(false);
+
+      // On the real robot, the right side sensor is already in phase but
+      // the right side output needs to be inverted so that positive is forward.
+      rightTalon1.setInverted(InvertType.InvertMotorOutput);
+      rightTalon1.setSensorPhase(false);
+    } else {
+      // Drive simulator expects positive motor outputs for forward
+      // and returns positive encoder values, so we don't need to
+      // invert or set sensor phase.
+      leftTalon1.setInverted(InvertType.None);
+      leftTalon1.setSensorPhase(false);
+      rightTalon1.setInverted(InvertType.None);
+      rightTalon1.setSensorPhase(false);
+    }
+
     // Sets the distance per pulse for the encoders
-    //m_leftEncoder.setDistancePerPulse(DriveConstants.kEncoderDistancePerPulse);
-  //  m_rightEncoder.setDistancePerPulse(DriveConstants.kEncoderDistancePerPulse);
+    // m_leftEncoder.setDistancePerPulse(DriveConstants.kEncoderDistancePerPulse);
+    // m_rightEncoder.setDistancePerPulse(DriveConstants.kEncoderDistancePerPulse);
     leftTalon1.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
     rightTalon1.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
-    
+
     resetEncoders();
     m_odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(getHeading()));
 
     if (RobotBase.isSimulation()) { // If our robot is simulated
       // This class simulates our drivetrain's motion around the field.
-      m_drivetrainSimulator =
-          new DifferentialDrivetrainSim(
-              DriveConstants.kDrivetrainPlant,
-              DriveConstants.kDriveGearbox,
-              DriveConstants.kDriveGearing,
-              DriveConstants.kTrackwidthMeters,
-              DriveConstants.kWheelDiameterMeters / 2.0,
-              VecBuilder.fill(0, 0, 0.0001, 0.1, 0.1, 0.005, 0.005));
+      m_drivetrainSimulator = new DifferentialDrivetrainSim(DriveConstants.kDrivetrainPlant,
+          DriveConstants.kDriveGearbox, DriveConstants.kDriveGearing, DriveConstants.kTrackwidthMeters,
+          DriveConstants.kWheelDiameterMeters / 2.0, VecBuilder.fill(0, 0, 0.0001, 0.1, 0.1, 0.005, 0.005));
 
       // The encoder and gyro angle sims let us set simulated sensor readings
-    //  m_leftEncoderSim = new EncoderSim(m_leftEncoder);
-    //  m_rightEncoderSim = new EncoderSim(m_rightEncoder);
+      // m_leftEncoderSim = new EncoderSim(m_leftEncoder);
+      // m_rightEncoderSim = new EncoderSim(m_rightEncoder);
       m_gyroSim = new ADXRS450_GyroSim(m_gyro);
 
       // the Field2d class lets us visualize our robot in the simulation GUI.
@@ -125,30 +157,28 @@ new SpeedControllerGroup(rightTalon1,rightTalon2,rightTalon3);
   @Override
   public void periodic() {
     // Update the odometry in the periodic block
-    m_odometry.update(
-        m_gyro.getRotation2d(),
-                      nativeUnitsToDistanceMeters(leftTalon1.getSelectedSensorPosition()),
-                      nativeUnitsToDistanceMeters(rightTalon1.getSelectedSensorPosition()));
+    m_odometry.update(m_gyro.getRotation2d(), nativeUnitsToDistanceMeters(leftTalon1.getSelectedSensorPosition()),
+        nativeUnitsToDistanceMeters(rightTalon1.getSelectedSensorPosition()));
     m_fieldSim.setRobotPose(getPose());
   }
 
-  private int distanceToNativeUnits(double positionMeters){
-    double wheelRotations = positionMeters/(2 * Math.PI * Units.inchesToMeters(kWheelRadiusInches));
+  private int distanceToNativeUnits(double positionMeters) {
+    double wheelRotations = positionMeters / (2 * Math.PI * Units.inchesToMeters(kWheelRadiusInches));
     double motorRotations = wheelRotations * kSensorGearRatio;
-    int sensorCounts = (int)(motorRotations * kCountsPerRev);
+    int sensorCounts = (int) (motorRotations * kCountsPerRev);
     return sensorCounts;
   }
 
-  private int velocityToNativeUnits(double velocityMetersPerSecond){
-    double wheelRotationsPerSecond = velocityMetersPerSecond/(2 * Math.PI * Units.inchesToMeters(kWheelRadiusInches));
+  private int velocityToNativeUnits(double velocityMetersPerSecond) {
+    double wheelRotationsPerSecond = velocityMetersPerSecond / (2 * Math.PI * Units.inchesToMeters(kWheelRadiusInches));
     double motorRotationsPerSecond = wheelRotationsPerSecond * kSensorGearRatio;
     double motorRotationsPer100ms = motorRotationsPerSecond / k100msPerSecond;
-    int sensorCountsPer100ms = (int)(motorRotationsPer100ms * kCountsPerRev);
+    int sensorCountsPer100ms = (int) (motorRotationsPer100ms * kCountsPerRev);
     return sensorCountsPer100ms;
   }
 
-  private double nativeUnitsToDistanceMeters(double sensorCounts){
-    double motorRotations = (double)sensorCounts / kCountsPerRev;
+  private double nativeUnitsToDistanceMeters(double sensorCounts) {
+    double motorRotations = (double) sensorCounts / kCountsPerRev;
     double wheelRotations = motorRotations / kSensorGearRatio;
     double positionMeters = wheelRotations * (2 * Math.PI * Units.inchesToMeters(kWheelRadiusInches));
     return positionMeters;
@@ -157,47 +187,28 @@ new SpeedControllerGroup(rightTalon1,rightTalon2,rightTalon3);
   @Override
   public void simulationPeriodic() {
     // To update our simulation, we set motor voltage inputs, update the simulation,
-    // and write the simulated positions and velocities to our simulated encoder and gyro.
+    // and write the simulated positions and velocities to our simulated encoder and
+    // gyro.
     // We negate the right side so that positive voltages make the right side
     // move forward.
-    m_drivetrainSimulator.setInputs(
-        leftTalon1.getMotorOutputVoltage(),
-        rightTalon1.getMotorOutputVoltage());
+    m_drivetrainSimulator.setInputs(leftTalon1.getMotorOutputVoltage(), rightTalon1.getMotorOutputVoltage());
     m_drivetrainSimulator.update(0.020);
 
-  //  m_leftEncoderSim.setDistance(m_drivetrainSimulator.getLeftPositionMeters());
-  //  m_leftEncoderSim.setRate(m_drivetrainSimulator.getLeftVelocityMetersPerSecond());
-  //  m_rightEncoderSim.setDistance(m_drivetrainSimulator.getRightPositionMeters());
-  //  m_rightEncoderSim.setRate(m_drivetrainSimulator.getRightVelocityMetersPerSecond());
-    /*m_leftMotorSim.setAnalogPosition((int)m_drivetrainSimulator.getLeftPositionMeters());
-    m_leftMotorSim.setAnalogVelocity((int)m_drivetrainSimulator.getLeftVelocityMetersPerSecond());
-    m_rightMotorSim.setAnalogPosition((int)m_drivetrainSimulator.getRightPositionMeters());
-    m_rightMotorSim.setAnalogVelocity((int)m_drivetrainSimulator.getRightVelocityMetersPerSecond()); */
-
-    m_leftMotorSim.setQuadratureRawPosition(
-      distanceToNativeUnits(
-        m_drivetrainSimulator.getLeftPositionMeters()));
-m_leftMotorSim.setQuadratureVelocity(
-      velocityToNativeUnits(
-        m_drivetrainSimulator.getLeftVelocityMetersPerSecond()));
-m_rightMotorSim.setQuadratureRawPosition(
-      distanceToNativeUnits(
-        m_drivetrainSimulator.getRightPositionMeters()));
-m_rightMotorSim.setQuadratureVelocity(
-      velocityToNativeUnits(
-        m_drivetrainSimulator.getRightVelocityMetersPerSecond()));
-m_gyroSim.setAngle(-m_drivetrainSimulator.getHeading().getDegrees());
-
-    
+    m_leftMotorSim.setQuadratureRawPosition(distanceToNativeUnits(m_drivetrainSimulator.getLeftPositionMeters()));
+    m_leftMotorSim.setQuadratureVelocity(velocityToNativeUnits(m_drivetrainSimulator.getLeftVelocityMetersPerSecond()));
+    m_rightMotorSim.setQuadratureRawPosition(distanceToNativeUnits(m_drivetrainSimulator.getRightPositionMeters()));
+    m_rightMotorSim
+        .setQuadratureVelocity(velocityToNativeUnits(m_drivetrainSimulator.getRightVelocityMetersPerSecond()));
     m_gyroSim.setAngle(-m_drivetrainSimulator.getHeading().getDegrees());
- 
+
     m_leftMotorSim.setBusVoltage(RobotController.getBatteryVoltage());
     m_rightMotorSim.setBusVoltage(RobotController.getBatteryVoltage());
   }
 
   /**
-   * Returns the current being drawn by the drivetrain. This works in SIMULATION ONLY! If you want
-   * it to work elsewhere, use the code in {@link DifferentialDrivetrainSim#getCurrentDrawAmps()}
+   * Returns the current being drawn by the drivetrain. This works in SIMULATION
+   * ONLY! If you want it to work elsewhere, use the code in
+   * {@link DifferentialDrivetrainSim#getCurrentDrawAmps()}
    *
    * @return The drawn current in Amps.
    */
@@ -220,8 +231,10 @@ m_gyroSim.setAngle(-m_drivetrainSimulator.getHeading().getDegrees());
    * @return The current wheel speeds.
    */
   public DifferentialDriveWheelSpeeds getWheelSpeeds() {
-  //  return new DifferentialDriveWheelSpeeds(m_leftEncoder.getRate(), m_rightEncoder.getRate());
-  return new DifferentialDriveWheelSpeeds(leftTalon1.getSelectedSensorVelocity(), rightTalon1.getSelectedSensorVelocity());
+    // return new DifferentialDriveWheelSpeeds(m_leftEncoder.getRate(),
+    // m_rightEncoder.getRate());
+    return new DifferentialDriveWheelSpeeds(leftTalon1.getSelectedSensorVelocity(),
+        rightTalon1.getSelectedSensorVelocity());
   }
 
   /**
@@ -248,7 +261,7 @@ m_gyroSim.setAngle(-m_drivetrainSimulator.getHeading().getDegrees());
   /**
    * Controls the left and right sides of the drive directly with voltages.
    *
-   * @param leftVolts the commanded left output
+   * @param leftVolts  the commanded left output
    * @param rightVolts the commanded right output
    */
   public void tankDriveVolts(double leftVolts, double rightVolts) {
@@ -264,9 +277,9 @@ m_gyroSim.setAngle(-m_drivetrainSimulator.getHeading().getDegrees());
 
   /** Resets the drive encoders to currently read a position of 0. */
   public void resetEncoders() {
-  //  m_leftEncoder.reset();
-  //  m_rightEncoder.reset();
-   // leftTalon1.
+    // m_leftEncoder.reset();
+    // m_rightEncoder.reset();
+    // leftTalon1.
   }
 
   /**
@@ -279,25 +292,26 @@ m_gyroSim.setAngle(-m_drivetrainSimulator.getHeading().getDegrees());
   }
 
   // /**
-  //  * Gets the left drive encoder.
-  //  *
-  //  * @return the left drive encoder
-  //  */
+  // * Gets the left drive encoder.
+  // *
+  // * @return the left drive encoder
+  // */
   // //public Encoder getLeftEncoder() {
-  //   return m_leftEncoder;
+  // return m_leftEncoder;
   // }
 
   // /**
-  //  * Gets the right drive encoder.
-  // //  *
-  // //  * @return the right drive encoder
-  // //  */
+  // * Gets the right drive encoder.
+  // // *
+  // // * @return the right drive encoder
+  // // */
   // public Encoder getRightEncoder() {
-  //   return m_rightEncoder;
+  // return m_rightEncoder;
   // }
 
   /**
-   * Sets the max output of the drive. Useful for scaling the drive to drive more slowly.
+   * Sets the max output of the drive. Useful for scaling the drive to drive more
+   * slowly.
    *
    * @param maxOutput the maximum output to which the drive will be constrained
    */
